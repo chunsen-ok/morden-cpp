8001efe7f6f6ef1912865d17116e040b3ee1dc6f
Acker <acker.liu@topibd.net>
2021-03-03 10:52:51 +0800

    mvc

---
+++ design_patterns/mvc.cpp
@@ -0,0 +1,16 @@
+// MVC 模式
+// Model - 业务逻辑与程序状态相关逻辑
+// View - 用户界面、内容展示；逻辑最小化，任何逻辑都应与内容展示相关
+// Controller - 处理用户输入和交互，负责选择模型和视图；控制应用如何响应请求
+#include <iostream>
+
+class Model {};
+
+class View {};
+
+class Controller {};
+
+int main() {
+    // todo ...
+    return 0;
+}
