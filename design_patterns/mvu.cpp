122617dc582907c5f0af55331407dde67d3512ce
Acker <acker.liu@topibd.net>
2021-03-18 15:26:10 +0800

    update

---
+++ design_patterns/mvu.cpp
@@ -0,0 +1,18 @@
+// MVU
+// View - [messages] ->
+#include <iostream>
+
+// Program, View
+class Todos {};
+
+// Model, State, Data
+class Model {};
+
+void init() {}
+void update() {}
+void view() {}
+
+int main() {
+    Todos todos;
+    return 0;
+}
