12299cba6028250fcb8e24784788b0c5f8ba967d
Acker <acker.liu@topibd.net>
2021-03-23 17:46:50 +0800

    update

---
+++ design_patterns/structure/adapter.cpp
@@ -0,0 +1,12 @@
+// 适配器模式
+// 使用外部、第三方库、接口时，无法修改其
+#include <iostream>
+
+class ThirdPartyProgram {
+public:
+
+};
+
+int main() {
+    return 0;
+}
--- design_patterns/structure/adapter.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MCPP_ADAPTER_HPP
-#define MCPP_ADAPTER_HPP
-
-namespace mcpp {
-    // TODO ...
-}
-
-#endif
