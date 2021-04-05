0ecdb6901cc26edd529400111155978fe54c053d
chuns <chuns.liu@foxmail.com>
2021-04-05 23:43:21 +0800

    update

--- algorithms/sort.hpp
+++ algorithms/sort.hpp
@@ -133,7 +133,6 @@ namespace mcpp {
         }
     }
 
-    // ...
 }
 
 #endif
--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+
+CMAKE_APP="D:/Qtz/Tools/CMake_64/bin/cmake.exe"
+
+if [ "$1" == "--build" ];  then
+    $CMAKE_APP -S "$2" -B "build/$2" -G "MinGW Makefiles"
+    $CMAKE_APP --build "build/$2"
+    $CMAKE_APP --install "build/$2"
+elif [ "$1" == "--run" ]; then
+    "bin/$2.exe"
+fi
