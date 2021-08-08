cc7e50c539b37f68212465f9847a8d3aa2bd95b5
chuns <chuns.liu@foxmail.com>
2021-08-08 12:01:10 +0800

    type convertions

--- robot.sh
+++ robot.sh
@@ -6,6 +6,11 @@ if [ "$1" == "--build" ];  then
     $CMAKE_APP -S "$2" -B "build/$2" -G "MinGW Makefiles"
     $CMAKE_APP --build "build/$2"
     $CMAKE_APP --install "build/$2"
+elif [ "$1" == "--new" ]; then
+    if [ -n "$2" ]; then
+        mkdir $2
+        cp project.template $2/CMakeLists.txt
+    fi
 elif [ "$1" == "--run" ]; then
     "bin/$2.exe"
 fi
---
+++ type_convertions/main.cpp
@@ -0,0 +1,15 @@
+// C++ 类型转换
+// 
+// 标准转换 - 隐式类型转换
+// const_cast
+// static_cast
+// dynmaic_cast
+// reinterpret_cast
+// 显式转换
+// 用户定义转换
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
