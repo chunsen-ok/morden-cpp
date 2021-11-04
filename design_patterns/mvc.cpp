daa3e05978eefe9083d2c60b3a94d274d741d944
liuchunsen <chuns.liu@foxmail.com>
2021-11-04 17:57:26 +0800

    update

--- design_patterns/mvc.cpp
+++ design_patterns/mvc.cpp
@@ -4,11 +4,54 @@
 // Controller - 处理用户输入和交互，负责选择模型和视图；控制应用如何响应请求
 #include <iostream>
 
-class Model {};
+namespace model {
 
-class View {};
+class Account;
+class ChatList;
+class Chatting;
+class UserPool;
+class FriendList;
+class GroupList;
+class MessageList;
+class ChattingGroupMemberList;
+class SearchResultList;
 
-class Controller {};
+class Model
+{
+public:
+    Account account();
+    ChatList chats();
+    Chatting chatting();
+    UserPool users();
+    FriendList friends();
+    GroupList groups();
+    MessageList messages();
+    ChattingGroupMemberList chattingGroupMembers();
+    SearchResultList searchResults();
+};
+
+}
+
+namespace view {
+
+class ChatList
+{
+public:
+    ChatList();
+};
+
+class View
+{
+public:
+
+};
+
+}
+
+class Controller {
+public:
+    void update(Model model, int role);
+};
 
 int main() {
     // todo ...
---
+++ play/data_roles.hpp
@@ -0,0 +1,10 @@
+#ifndef DATA_ROLES_HPP
+#define DATA_ROLES_HPP
+
+namespace play {
+namespace role {
+
+}
+}
+
+#endif
