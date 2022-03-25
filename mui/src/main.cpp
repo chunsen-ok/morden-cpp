501231188173c2928a99945e2c3afc24e109e9b7
liuchunsen <chuns.liu@foxmail.com>
2022-03-25 17:44:12 +0800

    update

--- mui/src/main.cpp
+++ mui/src/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include <vector>
 
-class BDataStore
-{
-public:
-    void dispatch();
-};
+// action的管理，同步，异步支持。
+class Store {};
 
-class BDataService{};
+// 管理数据获取、存储
+// 通知数据变更
+class Data {};
+
+// 负责业务逻辑，从Data获取需要的数据
+// Action支持组合
+class Action {};
+
+// 发送Action
+// 从Data获取数据显示
+// 监听Data变更
+class Presenter {};
 
 int main() {
-    std::vector<int> v{1,2,3,34};
-
-    auto it = std::prev(v.begin());
-    std::cout << (it == v.begin()) << (it == v.end()) << std::endl;
+    std::cout << 0xfff << std::endl;
     return 0;
 }
