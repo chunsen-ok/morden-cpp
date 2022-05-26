3c343466acfe06df75543101ff18b43327cf601c
liuchunsen <chuns.liu@foxmail.com>
2022-05-26 17:45:37 +0800

    update

--- mui/src/main.cpp
+++ mui/src/main.cpp
@@ -1,22 +1,75 @@
+#include <vector>
+#include <memory>
 #include <iostream>
+#include <unordered_map>
 
-// action的管理，同步，异步支持。
-class Store {};
+using UserID = int;
 
-// 管理数据获取、存储
-// 通知数据变更
-class Data {};
+enum class UserRole
+{
+    Id,
+    Name,
+    Avatar,
+};
 
-// 负责业务逻辑，从Data获取需要的数据
-// Action支持组合
-class Action {};
+struct UserItem
+{
+    using Role = UserRole;
 
-// 发送Action
-// 从Data获取数据显示
-// 监听Data变更
-class Presenter {};
+    UserID id;
+    std::string name;
+    std::string avatar;
+};
+
+class Users
+{
+public:
+    User user(UserID id) const;
+};
+
+enum class GroupRole
+{
+    Name,
+    Avatar,
+};
+
+struct GroupItem
+{
+    using Role = GroupRole;
+    
+    std::string name;
+    std::string avatar;
+};
+
+enum class ChatViewRole
+{
+    Name,
+    Avatar,
+};
+
+struct ChatViewItem
+{
+    std::string name;
+    std::string avatar;
+
+    template<typename Data>
+    void update(typename Data::Role role, const Data &data)
+    {
+        if constexpr (std::is_same_v<Data, UserItem>) {
+
+        } else if constexpr (std::is_same_v<Data, GroupItem>) {
+
+        }
+    }
+};
 
 int main() {
-    std::cout << 0xfff << std::endl;
+    auto user = UserItem{};
+    auto group = GroupItem{};
+    auto v = ChatViewItem{};
+
+    v.update(UserRole::Name, user);
+    v.update(GroupRole::Name, group);
+
     return 0;
 }
