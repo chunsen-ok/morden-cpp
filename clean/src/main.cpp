a6388ab73b320d7a5e267322561baa08137cd5fb
chuns <chuns.liu@foxmail.com>
2022-08-07 21:24:07 +0800

    update

--- clean/src/main.cpp
+++ clean/src/main.cpp
@@ -3,51 +3,231 @@
 #include <memory>
 #include <vector>
 
-class Canvas: public std::enable_shared_from_this<Canvas>
+enum class ElementType
+{
+    Unknown = 0,
+    Event,
+    One,
+    Two,
+};
+
+class AbstractElementData
 {
 public:
-    using Ptr = std::shared_ptr<Canvas>;
-    using WeakPtr = std::weak_ptr<Canvas>;
+    explicit AbstractElementData(ElementType type)
+        : mType{type}
+        , mId{0}
+    {}
+
+    AbstractElementData(ElementType type, int id)
+        : mType{type}
+        , mId{id}
+    {}
+
+    virtual ~AbstractElementData() = default;
 
 public:
-    Canvas() {}
-    virtual ~Canvas() = default;
+    ElementType type() const { return mType; }
+    int id() const { return mId; }
 
-    void append_element(const std::shared_ptr<Element> &el)
+private:
+    ElementType mType;
+    int mId;
+};
+
+class OneData: public AbstractElementData
+{
+public:
+    OneData(int one, int id)
+        : AbstractElementData{ElementType::One, id}
+        , mOne{one}
+    {}
+
+    int one() const { return mOne; }
+    void set_one(int one) { mOne = one; }
+
+private:
+    int mOne;
+};
+
+class TwoData: public AbstractElementData
+{
+public:
+    TwoData(int two, int id)
+        : AbstractElementData{ElementType::Two, id}
+        , mTwo{two}
+    {}
+
+    int two() const { return mTwo; }
+    void set_two(int two) { mTwo = two; }
+
+private:
+    int mTwo;
+};
+
+class EventData: public AbstractElementData
+{
+public:
+    EventData(const std::string &text, ElementType type, int id)
+        : AbstractElementData{type, id}
+        , mText{text}
+    {}
+
+    const std::string &text() const { return mText; }
+
+private:
+    std::string mText;
+};
+
+class AbstractElement
+{
+public:
+    virtual ~AbstractElement() = default;
+
+    virtual ElementType type() const = 0;
+    virtual int id() const = 0;
+
+    virtual void paint() {}
+};
+
+template<typename T>
+class Element: public AbstractElement, public T
+{
+public:
+    template<typename ... Args>
+    Element(Args ... args)
+        : AbstractElement()
+        , T(std::forward<Args>(args)...)
+    {}
+
+    ElementType type() const override
     {
-        if (el) {
-            m_children.push_back(el);
-        }
+        return T::type(); 
+    }
+    
+    int id() const override
+    {
+        return T::id();
+    }
+};
+
+template<typename T>
+class TipElement: public Element<T>
+{
+public:
+    template<typename ... Args>
+    TipElement(Args ... args)
+        : Element{std::forward<Args>(args)...}
+    {}
+
+    void paint() override final
+    {
+        std::cout << "Tip: " << text() << std::endl;
     }
 
 protected:
-     std::vector<std::shared_ptr<Element>> m_children;
+    virtual const std::string &text() const = 0;
 };
 
-class Element: public std::enable_shared_from_this<Element>
+template<typename T>
+class ChatElement: public Element<T>
 {
 public:
-    using Ptr = std::shared_ptr<Element>;
-    using WeakPtr = std::shared_ptr<Element>;
-
-    explicit Element(const Canvas::Ptr& canvas)
-        : m_canvas{canvas}
+    template<typename ... Args>
+    ChatElement(Args ... args)
+        : Element(std::forward<Args>(args)...)
     {}
 
-    virtual ~Element()
+    void paint() override final
     {
-        std::cout << "~Element\n";
+        std::cout << "Paint chat\n";
+        paintContent();
+    }
+
+protected:
+    virtual void paintContent() = 0;
+};
+
+class OneElement: public ChatElement<OneData>
+{
+public:
+    OneElement(int one, int id)
+        : ChatElement<OneData>{one, id}
+    {}
+
+protected:
+    void paintContent() override
+    {
+        std::cout << "paint one: " << one() << std::endl;
+    }
+};
+
+class TwoElement: public ChatElement<TwoData>
+{
+public:
+    TwoElement(int two, int id)
+        : ChatElement<TwoData>{two, id}
+    {}
+    
+    ~TwoElement()
+    {
+        std::cout << "~TwoElement\n";
+    }
+
+public:
+    void paintContent() override
+    {
+        std::cout << "paint two: " << two() << std::endl;
+    }
+};
+
+class EventElement: public TipElement<EventData>
+{
+public:
+    EventElement(const std::string &text, int id)
+        : TipElement<EventData>{text, ElementType::Event, id}
+    {}
+
+protected:
+    const std::string &text() const { return EventData::text(); }
+};
+
+class MessageHistory
+{
+public:
+    using Element = std::shared_ptr<AbstractElement>;
+
+    template<typename T, typename ... Args>
+    std::shared_ptr<T> makeMessage(Args ... args)
+    {
+        auto msg = std::make_shared<T>(std::forward<Args>(args)...);
+        insert(msg);
+        return msg; 
+    }
+
+    void insert(const Element &data)
+    {
+        mElements.push_back(data);
+    }
+
+    void paint()
+    {
+        for (const auto &ele: mElements)
+        {
+            ele->paint();
+        }
     }
 
 private:
-    Element::WeakPtr m_parent;
-    Canvas::WeakPtr  m_canvas;
+    std::vector<Element> mElements;
 };
 
 int main()
 {
-    auto canvas = std::make_shared<Canvas>();
+    MessageHistory history;
+    history.makeMessage<OneElement>(1124, 1);
+    history.makeMessage<TwoElement>(222222, 2);
+    history.makeMessage<EventElement>("Hello, world!", 3);
 
-    auto el0 = std::make_shared<Element>(canvas);
-    canvas->append_element(el0);
+    history.paint();
 }
