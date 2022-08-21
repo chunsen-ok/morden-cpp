94215fd1d8bf0058a6c594899c5a3be24a75935b
chuns <chuns.liu@foxmail.com>
2022-08-21 21:58:54 +0800

    update

--- cpp20/src/main.cpp
+++ cpp20/src/main.cpp
@@ -1,19 +1,56 @@
+#include <unordered_set>
 #include <iostream>
+
+class Edge;
+
+class Vertex
+{
+public:
+    void add_edge(const std::shared_ptr<Edge>& edge)
+    {
+        if (auto it = mEdges.find(edge); it == mEdges.end()) {
+            mEdges.insert(edge);
+        }
+    }
+
+private:
+    std::unordered_set<std::shared_ptr<Edge>> mEdges;
+};
+
+class Edge
+{
+public:
+    static std::shared_ptr<Edge> create(const std::shared_ptr<Vertex>& v0, const std::shared_ptr<Vertex>& v1)
+    {
+        auto edge = std::make_shared<Edge>(v0, v1);
+        v0->add_edge(edge);
+        v1->add_edge(edge);
+    }
+
+protected:
+    Edge(const std::shared_ptr<Vertex>& v0, const std::shared_ptr<Vertex>& v1)
+        : v0(v0)
+        , v1(v1)
+    {
+        
+    }
+
+private:
+    std::weak_ptr<Vertex> v0;
+    std::weak_ptr<Vertex> v1;
+};
+
+class Graph
+{
+public:
+    
+};
 
 int main()
 {
-    double foo = -0.0;
-    double bar = 0.0;
- 
-    auto res = foo <=> bar;
- 
-    if (res < 0)
-        std::cout << "-0 is less than 0";
-    else if (res > 0)
-        std::cout << "-0 is greater than 0";
-    else if (res == 0)
-        std::cout << "-0 and 0 are equal";
-    else
-        std::cout << "-0 and 0 are unordered";
+    auto v0 = std::make_shared<Vertex>();
+    auto v1 = std::make_shared<Vertex>();
+    Edge::create(v0, v1);
+
     return 0;
 }
