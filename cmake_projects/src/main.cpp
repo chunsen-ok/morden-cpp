89313d5164116d05e43d7be00284bba059d66e01
liuchunsen <chuns.liu@foxmail.com>
2022-08-24 18:00:50 +0800

    update

--- cmake_projects/src/main.cpp
+++ cmake_projects/src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <array>
+#include <cstring>
+
+class DataKey
+{
+public:
+    friend bool operator==(const DataKey& lhs, const DataKey& rhs);
+
+private:
+    char buf[24];
+};
+
+bool operator==(const DataKey& lhs, const DataKey& rhs)
+{
+    return std::memcmp(lhs.buf, rhs.buf, 24) == 0;
+}
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    DataKey k0;
+    DataKey k1;
+    std::cout << (k0 == k1) << std::endl;
+
     return 0;
 }
