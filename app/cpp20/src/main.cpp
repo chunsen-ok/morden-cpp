413861fec34b135c03545e4102360e1e17837429
liuchunsen <chuns.liu@foxmail.com>
2022-12-20 17:32:18 +0800

    update

--- app/cpp20/src/main.cpp
+++ app/cpp20/src/main.cpp
@@ -1,19 +1,94 @@
 #include <iostream>
 #include <vector>
+#include <variant>
+#include <ranges>
+#include <format>
 
-template<typename T>
-concept Addable = requires (T a, T b)
+class CanvasItem: public std::enable_shared_from_this<CanvasItem>
 {
-    a + b;
+public:
+    static std::shared_ptr<CanvasItem> create(double x, double y, double w, double h)
+    {
+        return std::shared_ptr<CanvasItem>(new CanvasItem(x,y,w,h));
+    }
+
+    double x() const { return mX; }
+    double y() const { return mY; }
+    double width() const { return mWidth; }
+    double height() const { return mHeight; }
+
+protected:
+    CanvasItem(double x, double y, double w, double h)
+        : mX(x), mY(y), mWidth(w), mHeight(h)
+    {}
+
+private:
+    double mX;
+    double mY;
+    double mWidth;
+    double mHeight;
 };
 
-template<typename T> requires Addable<T>
-T add(T a, T b)
+class BucketItem
 {
-    return a + b;
-}
+public:
+
+private:
+    std::weak_ptr<CanvasItem> mBucketItem;
+};
+
+class Bucket: public std::enable_shared_from_this<Bucket>
+{
+public:
+    static std::shared_ptr<Bucket> create()
+    {
+        return std::shared_ptr<Bucket>(new Bucket);
+    }
+
+    double baseMin() const;
+    double baseMax() const;
+    double min() const;
+    double max() const;
+    void insert(const BucketItem& item);
+    std::vector<BucketItem> intersected(double x) const;
+    void insertIntersects(const std::vector<BucketItem>& items);
+
+protected:
+    Bucket() {}
+};
+
+class BucketLayout
+{
+public:
+    void clear() { mBuckets.clear(); }
+
+    std::shared_ptr<Bucket> bucketAt(double x) const
+    {
+        // ...
+    }
+
+
+    void layout(const std::shared_ptr<CanvasItem>& data)
+    {
+        const auto x = data->x();
+
+    }
+
+private:
+    std::vector<std::shared_ptr<Bucket>> mBuckets;
+};
 
 int main() {
-    std::cout << add(1,2) << std::endl;
+    std::vector<std::shared_ptr<CanvasItem>> datas;
+    datas.push_back(CanvasItem::create(0,0,10,10));
+    datas.push_back(CanvasItem::create(20,0,10,10));
+    datas.push_back(CanvasItem::create(30,0,10,10));
+
+    BucketLayout layout;
+    layout.clear();
+    for (const auto& data: datas) {
+        layout.layout(data);
+    }
+
     return 0;
 }
