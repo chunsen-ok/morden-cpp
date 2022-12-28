ea9c9d71b7fe342c623104d342a59d4e3d0c5938
chuns <chuns.liu@foxmail.com>
2022-12-28 21:32:01 +0800

    update

--- app/cpp20/src/main.cpp
+++ app/cpp20/src/main.cpp
@@ -1,19 +1,69 @@
 #include <iostream>
-#include <vector>
 
 template<typename T>
-concept Addable = requires (T a, T b)
-{
-    a + b;
+struct AccTraits;
+
+template<>
+struct AccTraits<char> {
+    using AccT = int;
+    static constexpr AccT zero() { return 0; }
 };
 
-template<typename T> requires Addable<T>
-T add(T a, T b)
+template<>
+struct AccTraits<int> {
+    using AccT = long;
+    static constexpr AccT zero() { return 0; }
+};
+
+template<>
+struct AccTraits<float> {
+    using AccT = double;
+    static constexpr AccT zero() { return 0.0; }
+};
+
+struct SumPolicy {
+    template<typename T0, typename T1>
+    static T0 accumulate(T0 const& total, T1 const& value) {
+        return total + value;
+    }
+};
+
+struct MultiplyPolicy {
+    template<typename T0, typename T1>
+    static T0 accumulate(T0 const& total, T1 const& value) {
+        return total * value;
+    }
+};
+
+template<typename T>
+struct MaxTraits;
+
+template<>
+struct MaxTraits<char> {
+    using AccT = char;
+    static constexpr char zero() { return std::numeric_limits<char>::min(); }
+};
+
+struct MaxPolicy {
+    template<typename T0, typename T1>
+    static T0 accumulate(T0 const& total, T1 const& value) {
+        return std::max(total, value);
+    }
+};
+
+template<typename Iter, typename Policy = SumPolicy, typename AT = AccTraits<typename std::iterator_traits<Iter>::value_type>>
+auto accum(Iter beg, Iter end, typename AT::AccT const& initial = AT::zero())
 {
-    return a + b;
+    typename AT::AccT total = initial;
+    while (beg != end) {
+        total = Policy::accumulate(total, *beg);
+        ++beg;
+    }
+    return total;
 }
 
 int main() {
-    std::cout << add(1,2) << std::endl;
+    const char chars[] = "72985021734-2734.";
+    std::cout << accum<decltype(std::begin(chars)), MaxPolicy, MaxTraits<char>>(std::begin(chars), std::end(chars)) << std::endl;
     return 0;
 }
